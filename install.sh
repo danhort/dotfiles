8b6e9eb738796b3c8199c0334049be6313ee35ca
Daniel Horton-Wellings <danielhortonwellings@gmail.com>
2018-01-07 11:53:00 -0600

    first commit

---
+++ install.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# Get current dir (so run this script from anywhere)
+
+export DOTFILES_DIR
+DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# Make utilities available
+
+chmod -R u+x $DOTFILES_DIR/bin
+
+PATH="$DOTFILES_DIR/bin:$PATH"
+
+# Update dotfiles itself first
+
+if is-executable git -a -d "$DOTFILES_DIR/.git"; then git --work-tree="$DOTFILES_DIR" --git-dir="$DOTFILES_DIR/.git" pull origin master; fi
+
+# Bunch of symlinks
+
+ln -svf "$DOTFILES_DIR/runcom/.bash_profile" ~
+ln -svf "$DOTFILES_DIR/git/.gitconfig" ~DOTFILES_DIR
