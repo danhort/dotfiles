93f14a58ac4fc08ea545ccd38b99bc52f21857b5
Daniel Horton-Wellings <danielhortonwellings@gmail.com>
2021-09-27 17:08:15 -0500

    Update Ubuntu sofware install

--- software/install.sh
+++ software/install.sh
@@ -13,10 +13,15 @@ sudo add-apt-repository -y \
    $(lsb_release -cs) \
    stable"
 
-# Add github cli repository https://github.com/cli/cli/blob/trunk/docs/install_linux.md
-curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo apt-key add -
+# Add vscode
+curl -fsSL https://code.visualstudio.com/docs/?dv=linux64_deb | sudo apt-key add -
 sudo add-apt-repository -y \
-    "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main"
+    "[arch=amd64,arm64,armhf signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main"
+
+# Add github cli repository https://github.com/cli/cli/blob/trunk/docs/install_linux.md
+curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo gpg --dearmor -o /usr/share/keyrings/githubcli-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
+
 
 sudo apt update
 sudo apt install -y \
@@ -28,6 +33,7 @@ sudo apt install -y \
     htop \
     timewarrior \
     docker-ce \
+    code \
     gh \
     pv
 
